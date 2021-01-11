c44f72fe708440f42e0234ab0f8251e3f162f55d
alinavalovenko <alina.valovenko@gmail.com>
2021-01-11 22:16:24 +0200

    data added to vacancy template

--- wp-content/themes/job-hunting/inc/Entity/Vacancy.php
+++ wp-content/themes/job-hunting/inc/Entity/Vacancy.php
@@ -2,11 +2,12 @@
 
 namespace EcJobHunting\Entity;
 
+use EcJobHunting\Front\SiteSettings;
 use EcJobHunting\Service\User\UserService;
 
 class Vacancy
 {
-    private ?int $id= null;
+    private ?int $id = null;
     private string $title = '';
     private string $description = '';
     private ?string $datePosted = '';
@@ -14,8 +15,8 @@ class Vacancy
     private array $benefits = [];
     private string $currency = 'USD';
     private array $compensationRange = ['from' => 0, 'to' => 0];
-    private float $compensationMin = 0;
-    private float $compensationMax = 0;
+    private float $compensationFrom = 0;
+    private float $compensationTo = 0;
     private string $compensationPeriod = 'annualy';
     private bool $isCommissionIncluded = false;
 
@@ -25,6 +26,7 @@ class Vacancy
     private array $skills = []; //taxonomy Skills
 
     //employer info
+    private $employer;
     private string $streetAddress = '';
     private string $companyName = '';
     private string $reasonsToWork = '';
@@ -38,10 +40,10 @@ class Vacancy
     private int $candidatesNumber = 0;
     private int $visitors = 0;
 
+    //
+    private $fieldsObject;
 
-    private $employer;
-
-    public function __construct(?int $id= null)
+    public function __construct(?int $id = null)
     {
         $vacancy = get_post($id);
         if ($vacancy) {
@@ -49,30 +51,42 @@ class Vacancy
             $this->title = $vacancy->post_title;
             $this->description = strip_tags($vacancy->post_content);
             $this->author = $vacancy->post_author;
-
+            $this->fieldsObject = get_field_object('post_new_job', 'option');
 
             //Meta Data
-            $fields = get_field($id);
+            $fields = get_fields($id);
             if ($fields) {
-                $this->visitors = !empty($fields['visitors']) ? $fields['visitors'] : 0;
-                $this->candidates = !empty($fields['applied']) ? $fields['applied'] : [];
-                $this->candidatesNumber = count($this->candidates);
-                $this->benefits = !empty($fields['benefits']) ? $fields['benefits'] : [];
+                $this->compensationFrom = !empty($fields['compensation_range']['from']) ? $fields['compensation_range']['from'] : 0;
+                $this->compensationTo = !empty($fields['compensation_range']['to']) ? $fields['compensation_range']['to'] : 0;
                 $this->compensationRange = !empty($fields['compensation_range']) ? $fields['compensation_range'] : $this->compensationRange;
-                $this->location = !empty($fields['street_address']) ? $fields['street_address'] : '';
-                $this->companyName = !empty($fields['hiring_company']) ? $fields['hiring_company']
-                    : $this->employer->getName();
-                $this->reasonsToWork = !empty($fields['why_work_at_this_company']) ? $fields['why_work_at_this_company'] : '';
-                $this->companyDescription = !empty($fields['hiring_company_description']) ? $fields['hiring_company_description'] : '';
-                $this->emailsToNotify = !empty($fields['emails_to_inform']) ? $fields['emails_to_inform']
-                    : $this->employer->getEmail();
-                if (is_array($fields['additional_options'])) {
-                    $this->agreementOptions = in_array('closest', $fields['additional_options']) ?? false;
-                }
+                $this->streetAddress = !empty($fields['street_address']) ? $fields['street_address'] : '';
+
+                $this->companyName = $fields['hiring_company'] ?? '';
+                $this->reasonsToWork = $fields['why_work_at_this_company'] ?? '';
+                $this->companyDescription = $fields['hiring_company_description'] ?? '';
+                $this->notifyEmployer = (bool)$fields['emails_to_inform'];
+                $this->isCommissionIncluded = (bool)$fields['is_commission_included'];
+                $this->agreementOptions = $fields['additional_options'] ?? [];
+                $this->benefits = $fields['benefits'] ?? [];
+                $this->visitors = (int)$fields['visitors'] ?? 0;
+                $this->candidates = (array)$fields['applied'] ?? [];
+                $this->currency = ucwords($fields['compensation_currency'] ?? 'USD');
+                $this->compensationPeriod = $fields['compensation_period'] ?? 'annualy';
+
+                $this->employmentType = wp_get_post_terms($id, 'type', ['fields' => 'names']);
+                $this->location = wp_get_post_terms($id, 'location', ['fields' => 'names']);
             }
         } else {
             $this->employer = UserService::getUser();
         }
+    }
+
+    /**
+     * @return int|mixed
+     */
+    public function getVisitors()
+    {
+        return $this->visitors;
     }
 
     /**
@@ -84,75 +98,19 @@ class Vacancy
     }
 
     /**
-     * @return string
-     */
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-
-    /**
-     * @return string
-     */
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    /**
-     * @return Candidate|Company|EcJobUser
-     */
-    public function getEmployer()
-    {
-        return $this->employer;
-    }
-
-    /**
      * @return mixed|string
      */
-    public function getEmail()
+    public function getReasonsToWork()
     {
-        return $this->emailsToNotify;
+        return $this->reasonsToWork;
     }
 
     /**
-     * @return array|mixed
+     * @return array|mixed|string
      */
-    public function getApplied()
+    public function getLocation()
     {
-        return $this->applied;
-    }
-
-    /**
-     * @return int|string|null
-     */
-    public function getAuthor()
-    {
-        return $this->author;
-    }
-
-    /**
-     * @return array|mixed
-     */
-    public function getBenefits()
-    {
-        return $this->benefits;
-    }
-
-    /**
-     * @return mixed|string
-     */
-    public function getCompanyDescription()
-    {
-        return $this->companyDescription;
-    }
-
-    /**
-     * @return mixed|string
-     */
-    public function getCompanyName()
-    {
-        return $this->companyName;
+        return implode(', ', $this->location);
     }
 
     /**
@@ -166,24 +124,174 @@ class Vacancy
     /**
      * @return mixed|string
      */
-    public function getLocation()
+    public function getCompanyName()
     {
-        return $this->location;
+        return $this->companyName;
     }
 
     /**
      * @return mixed|string
      */
-    public function getReasonsToWork()
+    public function getCompanyDescription()
     {
-        return $this->reasonsToWork;
+        return $this->companyDescription;
     }
 
     /**
-     * @return int|mixed
+     * @return array|mixed
+     */
+    public function getBenefits()
+    {
+        if (!empty($this->fieldsObject['value']['benefits']) && is_array(
+                $this->fieldsObject['value']['benefits']
+            )) {
+            $columns = array_column($this->fieldsObject['value']['benefits'], 'key');
+            $values = [];
+            foreach ($this->benefits as $benefit) {
+                $key = array_search($benefit, $columns);
+                if ($key !== false) {
+                    array_push($values, $this->fieldsObject['value']['benefits'][$key]['name']);
+                }
+            }
+            return $values;
+        }
+        return $this->benefits;
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * @return Candidate|Company|EcJobUser
+     */
+    public function getEmployer()
+    {
+        return $this->employer;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return array|bool
+     */
+    public function getAgreementOptions()
+    {
+        return $this->agreementOptions;
+    }
+
+    /**
+     * @return array|mixed
      */
     public function getCandidates()
     {
         return $this->candidates;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCandidatesNumber(): int
+    {
+        return $this->candidatesNumber;
+    }
+
+    /**
+     * @return float|int
+     */
+    public function getCompensationTo()
+    {
+        return is_numeric($this->compensationTo) ? number_format($this->compensationTo, 2, '.', '') : 0.00;
+    }
+
+    /**
+     * @return float|int
+     */
+    public function getCompensationFrom()
+    {
+        return is_numeric($this->compensationFrom) ? number_format($this->compensationFrom, 2, '.', '') : 0.00;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCompensationPeriodName(): string
+    {
+        if (!empty($this->fieldsObject['value']['compensation_period']) && is_array(
+                $this->fieldsObject['value']['compensation_period']
+            )) {
+            $key = array_search(
+                $this->compensationPeriod,
+                array_column($this->fieldsObject['value']['compensation_period'], 'key')
+            );
+            return $this->fieldsObject['value']['compensation_period'][$key]['name'];
+        }
+        return $this->compensationPeriod;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCurrency(): string
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDatePosted(): ?string
+    {
+        return $this->datePosted;
+    }
+
+    /**
+     * @return mixed|string
+     */
+    public function getEmailsToNotify()
+    {
+        return $this->emailsToNotify;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmploymentType(): string
+    {
+        return implode(', ', $this->employmentType);
+    }
+
+    /**
+     * @return array
+     */
+    public function getSkills(): array
+    {
+        return $this->skills;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreetAddress(): string
+    {
+        return $this->streetAddress;
     }
 }
--- wp-content/themes/job-hunting/single-vacancy.php
+++ wp-content/themes/job-hunting/single-vacancy.php
@@ -1,6 +1,93 @@
-<?php get_header(); ?>
-<?php if (have_posts()): the_post(); ?>
-    <?php the_title(); ?>
-    <?php else: ?>
-        <?php get_template_part('content-none'); endif; ?>
-    <?php get_footer();
+<?php
+
+/***
+ * Vacancy Template
+ */
+
+use EcJobHunting\Entity\Vacancy;
+
+$vacancy = new Vacancy(get_the_ID());
+get_header(); ?>
+    <main>
+        <?php if (have_posts() && $vacancy): the_post();
+        $benefits = $vacancy->getBenefits();
+        ?>
+            <section class="mt-3 my-md-5">
+                <div class="container">
+                    <div class="row justify-content-xl-between">
+                        <div class="col-12 col-xl-8">
+                            <h1 class="mb-md-3 mb-xl-5"><?php echo $vacancy->getTitle(); ?></h1>
+                            <div class="vacancy-header"><strong class="my-3">
+                                    <?php
+                                    if ($vacancy->getCompensationFrom() && $vacancy->getCompensationTo()):
+                                        echo sprintf(
+                                            __('%s to %s', 'ecjobhunting'),
+                                            $vacancy->getCompensationFrom(),
+                                            $vacancy->getCompensationTo()
+                                        );
+                                    else:
+                                        echo $vacancy->getCompensationFrom() ? : $vacancy->getCompensationTo();
+                                    endif;
+
+                                    echo ' ' . $vacancy->getCompensationPeriodName();
+                                    ?></strong>
+                                <span class="color-secondary"><?php echo $vacancy->getEmploymentType(); ?></span>
+                                <span class="color-secondary"><?php echo $vacancy->getLocation(); ?></span>
+                                <?php if ($benefits) : ?>
+                                    <strong class="mt-4 mb-2">Benefits Offered</strong>
+                                    <ul>
+                                        <?php foreach ($benefits as $benefit): ?>
+                                            <li><?php echo $benefit; ?></li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                <?php endif; ?>
+                            </div>
+                            <article class="mt-5">
+                                <h4>Why Work at This Company?</h4>
+                                <p class="mb-4">Inc 500 fastest growing companies</p>
+                                <p class="mb-4">The Penso Agency is looking to hire a select few business partners for
+                                    the fastest growing financial services sales organization in the United States. We
+                                    are looking for like-minded people who are coachable and have a proven track record
+                                    of integrity and willingness to think outside the box. Insurance industry experience
+                                    is not required but you must be willing to think and act like a business owner.</p>
+                                <h4>About The Penso Agency:</h4>
+                                <p class="mb-4">INNOVATION: We are a people and tech company developing a new model in a
+                                    world of traditional insurance sales. With an ever changing market and the
+                                    proliferation of social media our business model is more lucrative than ever
+                                    before.</p>
+                                <p class="mb-4">FINANCIAL INDEPENDENCE: We are passionate about creating an
+                                    entrepreneurial platform for both personal producers who desire an active six figure
+                                    income and builders who want to create a passive income stream where the sky is the
+                                    limit.</p>
+                                <p class="mb-4">LEADERSHIP: Our mission is to serve our agents by providing access to
+                                    warm leads and a simple, yet, sophisticated selling system coupled with unparalleled
+                                    support and leadership. We build leaders!</p>
+                                <p class="mb-4">PRODUCT PORTFOLIO: Our carriers and their products are selected from the
+                                    BEST in the industry and serve our primary markets of Mortgage Protection, Final
+                                    Expense, Annuities and Index Universal Life. Our top rated carriers include
+                                    Foresters, United Home Life, and American Amicable.</p>
+                            </article>
+                            <div class="vacancy-footer"><a class="btn btn-primary btn-lg" href="#">Apply Now</a><a
+                                        class="btn btn-outline-primary btn-lg" href="#">Am I Qualified?</a></div>
+                        </div>
+                        <div class="col-12 col-xl-3 my-5 d-md-flex d-xl-block">
+                            <div class="vacancy-image"><img src="images/penso.png" alt="vacancy"></div>
+                            <div class="vacancy-info">
+                                <h2 class="vacancy-company no-decor">The Penso Agency</h2><span class="color-secondary">Posted date: 2 days ago</span>
+                                <div class="social mt-4"><span>Share this job:</span>
+                                    <ul>
+                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-envelope-o"></i></a></li>
+                                        <li><a href="#"><i class="fa fa-chain"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        <?php else: ?>
+            <?php get_template_part('content-none'); endif; ?>
+    </main>
+<?php get_footer();
