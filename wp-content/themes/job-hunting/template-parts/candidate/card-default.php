b8a3ebe7bdb7a0595915a32bc09accb37b003325
alinavalovenko <alina.valovenko@gmail.com>
2021-01-20 22:08:50 +0200

    my candidate displayed

--- wp-content/themes/job-hunting/template-parts/candidate/card-default.php
+++ wp-content/themes/job-hunting/template-parts/candidate/card-default.php
@@ -1,9 +1,15 @@
 <?php
 
 use EcJobHunting\Service\User\UserService;
+
 global $post;
 $isFirst = $args['isFirst'] ?? false;
-$candidate = UserService::getUser($post->post_author);
+global $post;
+if ($args['candidate']) {
+    $candidate = $args['candidate'];
+} else {
+    $candidate = UserService::getUser($post->post_author);
+}
 ?>
     <div class="results-item <?php echo $isFirst ? 'mt-4' : ''; ?>">
         <div class="container-fluid">
@@ -11,8 +17,11 @@ $candidate = UserService::getUser($post->post_author);
                 <div class="col d-none d-md-block col-md-3 col-xl-2">
                     <div class="results-image"><img src="<?php echo $candidate->getPhoto(); ?>" alt="photo"></div>
                 </div>
-                <div class="col-12 col-md-6 col-xl-7 d-flex flex-wrap"><small><?php echo nicetime($post->post_modified); ?></small>
-                    <h4 class="color-primary"><?php echo $candidate->getName(); ?></h4><span class="results-country color-secondary"><?php echo $candidate->getLocation(); ?></span>
+                <div class="col-12 col-md-6 col-xl-7 d-flex flex-wrap"><small><?php echo nicetime(
+                            $post->post_modified
+                        ); ?></small>
+                    <h4 class="color-primary"><?php echo $candidate->getName(); ?></h4><span
+                            class="results-country color-secondary"><?php echo $candidate->getLocation(); ?></span>
                     <ul>
                         <li><span class="color-secondary">Applied to Graphic Designer</span></li>
                         <li class="color-black">Director <span class="color-secondary">at Galirie 255</span></li>
