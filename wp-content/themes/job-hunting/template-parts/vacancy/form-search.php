206627265861df0345a865225336c5aa2b01a101
Yevhenii Rodkin <rodkin.yevhenii@gmail.com>
2021-04-25 00:15:05 +0300

    Updated vacancy search form template

--- wp-content/themes/job-hunting/template-parts/vacancy/form-search.php
+++ wp-content/themes/job-hunting/template-parts/vacancy/form-search.php
@@ -3,16 +3,18 @@
         <div class="row d-flex justify-content-xl-center">
             <div class="col-12 col-md-5 col-xl-4">
                 <label class="my-2">
-                    <input class="field-text" type="text" placeholder="Job Title">
+                    <input class="field-text" type="text" placeholder="<?php _e('Job Title', 'ecjobhunting'); ?>">
                 </label>
             </div>
             <div class="col-12 col-md-5 col-xl-4">
                 <label class="my-2">
-                    <input class="field-text" type="text" placeholder="Location">
+                    <input class="field-text" type="text" placeholder="<?php _e('Location', 'ecjobhunting'); ?>">
                 </label>
             </div>
             <div class="col-12 col-md-2">
-                <button class="btn btn-primary my-2" type="submit">Search</button>
+                <button class="btn btn-primary my-2" type="submit">
+                    <?php _e('Search', 'ecjobhunting'); ?>
+                </button>
             </div>
         </div>
     </div>
