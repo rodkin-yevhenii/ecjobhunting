92d919a9ef7c7d94f2bc8fde4ac0955482713cee
Yevhenii Rodkin <rodkin.yevhenii@gmail.com>
2021-04-28 14:25:27 +0300

    Updated search form

--- wp-content/themes/job-hunting/template-parts/vacancy/form-search.php
+++ wp-content/themes/job-hunting/template-parts/vacancy/form-search.php
@@ -1,14 +1,31 @@
-<form class="hero">
+<?php
+$searchPageUrl = get_field('search_page_url', 'option');
+$s = $_GET['s'] ?? '';
+$location = $_GET['location'] ?? '';
+?>
+<form class="hero" method="get" action="<?php echo $searchPageUrl; ?>">
     <div class="container">
         <div class="row d-flex justify-content-xl-center">
             <div class="col-12 col-md-5 col-xl-4">
                 <label class="my-2">
-                    <input class="field-text" type="text" placeholder="<?php _e('Job Title', 'ecjobhunting'); ?>">
+                    <input
+                        class="field-text"
+                        type="text"
+                        name="s"
+                        placeholder="<?php _e('Job Title', 'ecjobhunting'); ?>"
+                        value="<?php echo $s; ?>"
+                    />
                 </label>
             </div>
             <div class="col-12 col-md-5 col-xl-4">
                 <label class="my-2">
-                    <input class="field-text" type="text" placeholder="<?php _e('Location', 'ecjobhunting'); ?>">
+                    <input
+                            class="field-text"
+                            type="text"
+                            name="location"
+                            placeholder="<?php _e('Location', 'ecjobhunting'); ?>"
+                            value="<?php echo $location; ?>"
+                    />
                 </label>
             </div>
             <div class="col-12 col-md-2">
