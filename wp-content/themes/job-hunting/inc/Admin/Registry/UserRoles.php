773c339d69ab5ab2c221478e40834bb673c56f65
Yevhenii Rodkin <rodkin.yevhenii@gmail.com>
2021-05-03 14:53:46 +0300

    Updated user roles

--- wp-content/themes/job-hunting/inc/Admin/Registry/UserRoles.php
+++ wp-content/themes/job-hunting/inc/Admin/Registry/UserRoles.php
@@ -12,29 +12,67 @@ class UserRoles
 
     private function registerEmployee()
     {
-        add_role(
+        $result = add_role(
             'candidate',
             'Candidate',
             [
                 'read' => true,
                 'edit_posts' => true,
+                'edit_published_posts' => true,
                 'upload_files' => true,
                 'publish_posts' => true,
             ]
         );
+
+        if ($result) {
+            return;
+        }
+
+        $this->updateRole('candidate');
     }
 
     private function registerEmployer()
     {
-        add_role(
+        $result = add_role(
             'employer',
             'Employer',
             [
                 'read' => true,
                 'edit_posts' => true,
+                'edit_published_posts' => true,
                 'upload_files' => true,
                 'publish_posts' => true,
             ]
         );
+
+        if ($result) {
+            return;
+        }
+
+        $this->updateRole('candidate');
+    }
+
+    public function updateRole(string $role): void {
+        $role = get_role($role);
+
+        if (!$role->has_cap('read')) {
+            $role->add_cap('read', true);
+        }
+
+        if (!$role->has_cap('edit_posts')) {
+            $role->add_cap('edit_posts', true);
+        }
+
+        if (!$role->has_cap('edit_published_posts')) {
+            $role->add_cap('edit_published_posts', true);
+        }
+
+        if (!$role->has_cap('upload_files')) {
+            $role->add_cap('upload_files', true);
+        }
+
+        if (!$role->has_cap('publish_posts')) {
+            $role->add_cap('publish_posts', true);
+        }
     }
 }
