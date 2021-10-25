8a3da942aca2dae7ea4b1fd4232a5b2770af81a8
Yevhenii Rodkin <rodkin.yevhenii@gmail.com>
2021-10-25 20:42:24 +0300

    Updated WP & Plugins

---
+++ wp-content/themes/job-hunting/inc/Service/Email/Email.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EcJobHunting\Service\Email;
+
+/**
+ * Class Email
+ *
+ * @author Yevhenii Rodkin <rodkin.yevhenii@gmail.com>
+ * @package EcJobHunting\Service\Email
+ */
+class Email
+{
+    private string $from;
+    private string $type = 'text/html';
+
+    public function __construct()
+    {
+
+    }
+}
