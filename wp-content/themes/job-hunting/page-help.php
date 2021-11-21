543aedb3809e4cfdf4efef302aa7f37b16c57e5e
Yevhenii Rodkin <rodkin.yevhenii@gmail.com>
2021-11-21 12:44:11 +0200

    Updated employer help page template

--- wp-content/themes/job-hunting/page-help.php
+++ wp-content/themes/job-hunting/page-help.php
@@ -8,6 +8,23 @@
 $faq = get_field('faq');
 $contactsPageLink = get_field('contacts_page');
 
+$isShowPhoneEmail = get_field('is_show_help_phone_and_email') ?? false;
+$isShowFAQ = get_field('is_show_faq') ?? false;
+$isContactButton = get_field('is_show_contact_btn') ?? false;
+$phone = get_field('help_phone');
+$email = get_field('help_email');
+
+if (empty($phone)) {
+    $phone = get_field('site_phone', 'option');
+}
+
+if (empty($email)) {
+    $email = get_field('site_email', 'option');
+}
+
+$cleanedPhone = trim($phone);
+$cleanedPhone = str_replace([' ', '-', '(', ')'], '', $cleanedPhone);
+
 get_header();
 ?>
     <div class="page employer">
@@ -15,7 +32,23 @@ get_header();
             <div class="row">
                 <div class="col-12">
                     <h1><?php echo the_title(); ?></h1>
-                    <?php if (!empty($faq)) : ?>
+                </div>
+                <?php if ($isShowPhoneEmail && !empty($phone)) : ?>
+                    <div class="col-12 col-md-6 col-xl-4 pb-4">
+                        <p class="m-0"><?php _e('Our service is available by dialing', 'ecjobhunting'); ?>:</p>
+                        <a class="link-phone" href="tel:<?php echo $cleanedPhone; ?>"><?php echo $phone; ?></a>
+                    </div>
+                <?php endif;
+
+                if ($isShowPhoneEmail && !empty($email)) : ?>
+                    <div class="col-12 col-md-6 pb-4">
+                        <p class="m-0"><?php _e('Our email', 'ecjobhunting'); ?>:</p>
+                        <a class="link-email" href="mailto:<?php echo $email; ?>"><?php echo $email; ?></a>
+                    </div>
+                <?php endif; ?>
+
+                <div class="col-12">
+                    <?php if ($isShowFAQ && !empty($faq)) : ?>
                         <div class="accordion" id="help">
                             <?php
                             $counter = 1;
@@ -48,7 +81,7 @@ get_header();
                     <?php endif; ?>
                 </div>
             </div>
-            <?php if (!empty($contactsPageLink)) : ?>
+            <?php if ($isContactButton && !empty($contactsPageLink)) : ?>
                 <div class="row">
                     <div class="col-12 mt-4 mt-md-5">
                         <p class="mt-0 text-large text-center">Still need help?</p>
